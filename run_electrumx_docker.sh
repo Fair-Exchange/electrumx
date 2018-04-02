c70d642ba54a6f46a2fa805749bb2a6893feddd3
nimbosa <darth.Serious@gmail.com>
2018-04-02 12:09:49 +0800

    2018-04-02 UPDATE: libleveldb1=>libleveldb1v5, rpcport 8232=>8023
    replace libleveldb1 with libleveldb1v5
    use new rpcport 8023 instead of 8232

--- run_electrumx_docker.sh
+++ run_electrumx_docker.sh
@@ -16,4 +16,4 @@ mkdir -p ~/.zclassic/
 cp zclassic.conf ~/.zclassic/
 /home/zcluser/zclassic/src/zcashd -daemon
 
-COIN=Zclassic DB_DIRECTORY=/home/zcluser/zcl_electrum_db DAEMON_URL=http://${RPCUSER}:${RPCPASS}@127.0.0.1:8232 HOST=0.0.0.0 SSL_PORT=50002 PEER_DISCOVERY=Off SSL_CERTFILE=/home/zcluser/server.crt SSL_KEYFILE=/home/zcluser/server.key BANDWIDTH_LIMIT=10000000 /home/zcluser/electrumx/electrumx_server.py
+COIN=Zclassic DB_DIRECTORY=/home/zcluser/zcl_electrum_db DAEMON_URL=http://${RPCUSER}:${RPCPASS}@127.0.0.1:8023 HOST=0.0.0.0 SSL_PORT=50002 PEER_DISCOVERY=On SSL_CERTFILE=/home/zcluser/server.crt SSL_KEYFILE=/home/zcluser/server.key BANDWIDTH_LIMIT=10000000 /home/zcluser/electrumx/electrumx_server.py
