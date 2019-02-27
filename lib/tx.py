a1f6d70f88229aceabc31d0a325470bd7238ed04
mexican1973 <36118413+mexican1973@users.noreply.github.com>
2019-02-27 23:07:16 +0100

    Update tx.py
    Added sapling suppport according to James hint. Copied from kyuupichan's original elöectrumx code which was updated last year 2018 in October for Zcash.

--- lib/tx.py
+++ lib/tx.py
@@ -306,23 +306,53 @@ class TxJoinSplit(namedtuple("Tx", "version inputs outputs locktime")):
         return self.inputs[0].is_coinbase if len(self.inputs) > 0 else False
 
 
-class DeserializerZcash(DeserializerEquihash):
+class DeserializerZcash(DeserializerEquihash):  # added by HS from kyuupichan's github
     def read_tx(self):
-        base_tx =  TxJoinSplit(
-            self._read_le_int32(),  # version
+        header = self._read_le_uint32()
+        overwintered = ((header >> 31) == 1)
+        if overwintered:
+            version = header & 0x7fffffff
+            self.cursor += 4  # versionGroupId
+        else:
+            version = header
+
+        is_overwinter_v3 = version == 3
+        is_sapling_v4 = version == 4
+
+        base_tx = TxJoinSplit(
+            version,
             self._read_inputs(),    # inputs
             self._read_outputs(),   # outputs
             self._read_le_uint32()  # locktime
         )
+
+        if is_overwinter_v3 or is_sapling_v4:
+            self.cursor += 4  # expiryHeight
+
+        has_shielded = False
+        if is_sapling_v4:
+            self.cursor += 8  # valueBalance
+            shielded_spend_size = self._read_varint()
+            self.cursor += shielded_spend_size * 384  # vShieldedSpend
+            shielded_output_size = self._read_varint()
+            self.cursor += shielded_output_size * 948  # vShieldedOutput
+            has_shielded = shielded_spend_size > 0 or shielded_output_size > 0
+
         if base_tx.version >= 2:
             joinsplit_size = self._read_varint()
             if joinsplit_size > 0:
-                self.cursor += joinsplit_size * 1802 # JSDescription
-                self.cursor += 32 # joinSplitPubKey
-                self.cursor += 64 # joinSplitSig
+                joinsplit_desc_len = 1506 + (192 if is_sapling_v4 else 296)
+                # JSDescription
+                self.cursor += joinsplit_size * joinsplit_desc_len
+                self.cursor += 32  # joinSplitPubKey
+                self.cursor += 64  # joinSplitSig
+
+        if is_sapling_v4 and has_shielded:
+            self.cursor += 64  # bindingSig
+
         return base_tx
 
-
+      
 class TxTime(namedtuple("Tx", "version time inputs outputs locktime")):
     '''Class representing transaction that has a time field.'''
 
